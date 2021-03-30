d980eb4373433d2b4571dcee25d4e32a2d9c92dc
Kevin McCormick <kevin.patrick.mccormick@gmail.com>
2021-03-30 21:22:54 +0000

    fixed topscore_api.sh

--- tools/topscore_api.sh
+++ tools/topscore_api.sh
@@ -7,4 +7,4 @@ query=$6
 options=$7
 curl -s $AUTH_URL -d "grant_type=client_credentials&client_id=$client_id&client_secret=$client_secret" > /dev/null
 access_token=$(curl -s $AUTH_URL -d "grant_type=password&client_id=$client_id&client_secret=$client_secret&username=$username&password=$password" | jq -r '.access_token')
-curl -s https://pada.usetopscore.com/api/$query -H "Authorization: Bearer $access_token | $options"
+curl -s https://$1.usetopscore.com/api/$query -H "Authorization: Bearer $access_token | $options"
